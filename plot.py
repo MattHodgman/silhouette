c1567a9a6018e5edc19d066d17dc283e01900573
Matt Hodgman <matthodg1@gmail.com>
2021-08-06 03:47:33 -0600

    init plot results

---
+++ plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import sys
+import pandas as pd
+
+# params: (1) scores.csv [k,score]
+
+# load data
+data = pd.read_csv(sys.argv[1])
+
+# plot
+plt.scatter(x=data['k'], y=data['score'])
+plt.show()
