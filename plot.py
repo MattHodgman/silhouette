f181ef107615939130ca57247d748320f12574a3
Matt Hodgman <matthodg1@gmail.com>
2021-08-06 04:01:10 -0600

    save to method dir

--- plot.py
+++ plot.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import sys
 import pandas as pd
 
-# params: (1) scores.csv [k,score] (2) method name
+# params: (1) scores.csv [k,score] (2) method name (name of output dir as well)
 
 # load data
 data = pd.read_csv(sys.argv[1])
-title = sys.arv[2]
+title = sys.argv[2]
 
 # plot
 plt.scatter(x=data['k'], y=data['score'])
@@ -14,4 +14,4 @@ plt.title(title)
 plt.xlabel('k')
 plt.ylabel('avg silhouette score')
 plt.show()
-plt.savefig(f'{title}_scores.png')
+plt.savefig(f'{title}/{title}_scores.png')
