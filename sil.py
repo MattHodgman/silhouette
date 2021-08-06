65f617ede5fbda565a7372626e3d016942977c79
Matt Hodgman <matthodg1@gmail.com>
2021-08-06 03:28:48 -0600

    use transformed data

---
+++ sil.py
@@ -0,0 +1,22 @@
+from silhouette import CELLID
+from sklearn.metrics import silhouette_score
+import pandas as pd
+import sys
+
+# params: (1) transformed feature table from MCMICRO (2) cluster labels
+
+# constants
+CELLID = 'CellID'
+CLUSTER = 'Cluster'
+
+# read input
+X = pd.read_csv(sys.argv[1], index_col=CELLID) # get data
+cluster_labels = pd.read_csv(sys.argv[2], index_col=CELLID)
+
+# logicle transform the data
+
+# calculate average silhouette score
+silhouette_avg = silhouette_score(X, cluster_labels[CLUSTER])
+
+# write score to stdout
+print(silhouette_avg)
